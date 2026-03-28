ef8885d6bce8d4825856c2597985772d99cf03ba
Bonnie Bingham <cashcam1@gmail.com>
2026-03-28 10:33:47 -0700

    Add PyPI packaging files - reveal-nlp 0.1.0

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="reveal-nlp",
+    version="0.1.0",
+    author="Bonnie Bingham",
+    description="Open-source Python library for intelligent harm detection and linguistic analysis in text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bbplus3/reveal",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "reveal": ["dictionaries/*.json"],
+    },
+    install_requires=[
+        "nltk>=3.8",
+        "textblob>=0.17",
+        "scikit-learn>=1.0",
+        "numpy>=1.19",
+        "pandas>=1.3",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Text Processing :: Linguistic",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+    ],
+    python_requires=">=3.9",
+    keywords="nlp harm-detection linguistic-analysis cryptanalysis propaganda sentiment",
+)
